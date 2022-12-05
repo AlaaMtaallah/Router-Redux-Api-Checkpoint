4831c14e7c5f7f81c7ea3f84d0480d8843d2dfb1
Alaa <ala.mtaallah@gmail.com>
2022-12-05 02:40:39 +0100

    first commit

---
+++ src/Components/ProductListing.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import Product from "./Product";
+import { useSelector, useDispatch } from "react-redux";
+import { setProducts } from "../Redux/Actions/productActions";
+
+const ProductListing = () => {
+    const products = useSelector((state) => state);
+    const dispatch = useDispatch();
+    const fetchProducts = async () => {
+        const response = await axios
+          .get("https://fakestoreapi.com/products")
+          .catch((err) => {
+            console.log("Err: ", err);
+          });
+          
+          dispatch(setProducts(response.data));
+      };
+    useEffect(() => {
+        fetchProducts();
+    },[]);
+    console.log("products: ", products)
+    return(
+        <div className="ui grid mt-4 container">
+            <Product />
+        </div>
+    );
+};
+
+export default ProductListing;
